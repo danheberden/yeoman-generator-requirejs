c506df8dc7e57797af877db9c992f8755f828f92
David Baumgold <david@davidbaumgold.com>
2013-07-04 09:45:37 -0400

    Bower 0.10.0 uses `bower_components` directory
    https://github.com/bower/bower/commit/292c0356f1ec0f6c70e9c51b4404257d2a6756f1

--- app/templates/app/main.js
+++ app/templates/app/main.js
@@ -1,5 +1,5 @@
 // to depend on a bower installed component:
-// define(['component/componentName/file'])
+// define(['bower_components/componentName/file'])
 
 define(["jquery"], function($) {
   $('body').append('jQuery ' + $.fn.jquery + ' loaded!');
